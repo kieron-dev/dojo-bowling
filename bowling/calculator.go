3359bacaa9882f2f86a9791d4a47966382e7102d
Kieron Browne <kbrowne@pivotal.io>
2017-12-07 18:34:33 +0000

    make functions more single responsibility

--- bowling/calculator.go
+++ bowling/calculator.go
@@ -18,8 +18,48 @@ type Frame struct {
 	FrameType FrameType
 }
 
+func NewStrike() Frame {
+	return Frame{Throw1: 10, FrameType: Strike}
+}
+
+func NewSpare(throw1 string) Frame {
+	score, _ := strconv.Atoi(throw1)
+	return Frame{
+		Throw1:    score,
+		Throw2:    10 - score,
+		FrameType: Spare,
+	}
+}
+
+func NewFrame(throws string) Frame {
+	t1, _ := strconv.Atoi(string(throws[0]))
+	frame := Frame{Throw1: t1}
+	if len(throws) < 2 {
+		return frame
+	}
+	t2, _ := strconv.Atoi(string(throws[1]))
+	frame.Throw2 = t2
+	return frame
+}
+
+func Throws2Frames(throws string) []Frame {
+	if len(throws) == 0 {
+		return []Frame{}
+	}
+	if string(throws[0]) == "X" {
+		return append([]Frame{NewStrike()}, Throws2Frames(throws[1:])...)
+	}
+	if len(throws) > 1 {
+		if throws[1] == '/' {
+			return append([]Frame{NewSpare(throws[0:1])}, Throws2Frames(throws[2:])...)
+		}
+		return append([]Frame{NewFrame(throws[:2])}, Throws2Frames(throws[2:])...)
+	}
+	return []Frame{NewFrame(throws)}
+}
+
 func Score(throws string) int {
-	frames := ParseFrames(throws)
+	frames := Throws2Frames(throws)
 	var sum int
 	for i := 0; i < 10 && i < len(frames); i++ {
 		sum += frames[i].Throw1 + frames[i].Throw2
@@ -35,35 +75,4 @@ func Score(throws string) int {
 		}
 	}
 	return sum
-}
-
-func ParseFrames(throws string) []Frame {
-	frames := []Frame{}
-	for len(throws) > 0 {
-		if throws[0] == 'X' {
-			frames = append(frames, Frame{Throw1: 10, FrameType: Strike})
-			throws = throws[1:]
-			continue
-		}
-
-		first, _ := strconv.Atoi(string(throws[0]))
-		var next int
-
-		frameType := Regular
-		var newFrame Frame
-		if len(throws) > 1 {
-			if throws[1] == '/' {
-				next = 10 - first
-				frameType = Spare
-			} else {
-				next, _ = strconv.Atoi(string(throws[1]))
-			}
-			throws = throws[2:]
-		} else {
-			throws = ""
-		}
-		newFrame = Frame{Throw1: first, Throw2: next, FrameType: frameType}
-		frames = append(frames, newFrame)
-	}
-	return frames
 }
--- bowling/calculator_test.go
+++ bowling/calculator_test.go
@@ -24,32 +24,38 @@ var _ = Describe("Calculator", func() {
 	})
 
 	It("can parse inputs into framelists", func() {
-		Expect(bowling.ParseFrames("34")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("34")).To(Equal([]bowling.Frame{
 			{Throw1: 3, Throw2: 4, FrameType: bowling.Regular},
 		}))
-		Expect(bowling.ParseFrames("-4")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("-4")).To(Equal([]bowling.Frame{
 			{Throw1: 0, Throw2: 4, FrameType: bowling.Regular},
 		}))
-		Expect(bowling.ParseFrames("3/")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("3/")).To(Equal([]bowling.Frame{
 			{Throw1: 3, Throw2: 7, FrameType: bowling.Spare},
 		}))
 
-		Expect(bowling.ParseFrames("225/")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("225/")).To(Equal([]bowling.Frame{
 			{Throw1: 2, Throw2: 2, FrameType: bowling.Regular},
 			{Throw1: 5, Throw2: 5, FrameType: bowling.Spare},
 		}))
 
-		Expect(bowling.ParseFrames("X")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("X")).To(Equal([]bowling.Frame{
 			{Throw1: 10, Throw2: 0, FrameType: bowling.Strike},
 		}))
-		Expect(bowling.ParseFrames("XX")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("XX")).To(Equal([]bowling.Frame{
 			{Throw1: 10, Throw2: 0, FrameType: bowling.Strike},
 			{Throw1: 10, Throw2: 0, FrameType: bowling.Strike},
 		}))
-		Expect(bowling.ParseFrames("X72X")).To(Equal([]bowling.Frame{
+		Expect(bowling.Throws2Frames("X72X")).To(Equal([]bowling.Frame{
 			{Throw1: 10, Throw2: 0, FrameType: bowling.Strike},
 			{Throw1: 7, Throw2: 2, FrameType: bowling.Regular},
 			{Throw1: 10, Throw2: 0, FrameType: bowling.Strike},
+		}))
+	})
+
+	It("Deals with a single number in a frame", func() {
+		Expect(bowling.NewFrame("5")).To(Equal(bowling.Frame{
+			Throw1: 5,
 		}))
 	})
 })
